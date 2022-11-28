79d4dc4237d8bce994189537d8058d3f71308114
phestrix <matrixx.panasx@gmail.com>
2022-11-28 22:14:33 +0700

    writed functions to parse config file

--- config_parser.cpp
+++ config_parser.cpp
@@ -1,38 +1,72 @@
 #include "config_parser.hpp"
 
+#include <algorithm>
 #include <cstring>
 
 void Parser_cfg::parse_config_file() {
   std::string buf;
-  char a[2] = "#";
-
-  std::getline(config_file, buf);
-  if (buf.at(0) == *a) {
-    buf.clear();
+  char a = '#';
+  while (config_file.good()) {
     std::getline(config_file, buf);
-  }
-  std::string mix = "mix";
-  std::string mute = "mute";
-  if (buf.find(mix)) {
-    read_stream_and_second(&buf);
-  }
-  if (buf.find(mute)) {
-    read_seconds(&buf);
+    if (buf.at(0) == a) {
+      buf.clear();
+      std::getline(config_file, buf);
+    }
+
+    std::string mix = "mix";
+    std::string mute = "mute";
+
+    // find number of stream and number, than push them to vector of pair
+    if (buf.find(mix)) {
+      std::vector<unsigned> res;
+      read_stream_and_second(&buf, &res);
+      std::string tmp = "input" + res.at(0);
+      stream_seconds_for_mix.push_back(std::make_pair(tmp, res.at(1)));
+      tmp.~basic_string();
+      res.clear();
+      buffer.push_back('x');
+    }
+
+    // find seconds to mute and push them in vector of seconds
+    if (buf.find(mute)) {
+      std::vector<unsigned> res;
+      read_seconds(&buf, &res);
+      mute_seconds.push_back(res.at(0));
+      mute_seconds.push_back(res.at(1));
+      res.clear();
+      buffer.push_back('x');
+    }
   }
 };
 
-std::vector<unsigned>* Parser_cfg::read_seconds(std::string* str) {
+std::vector<unsigned>* Parser_cfg::read_seconds(std::string* str,
+                                                std::vector<unsigned>* res) {
   if (str->find(" ")) {
     size_t pos = str->find(" ");
     if (str->find(" ", pos + 1)) {
       size_t interval = str->find(" ", pos + 1);
-      std::vector<unsigned> res;
       unsigned number = 0;
       unsigned pow = 1;
-      while (interval > pos) {
-        number += ((pow) * (unsigned)str->at(interval));
+      while (pos < interval) {
+        number += ((pow) * (unsigned)str->at(pos));
         pow *= 10;
-        --interval;
+        ++pos;
+      }
+      res->push_back(number);
+      if (str->find(" ", interval + 1)) {
+        pos = str->find(" ", interval + 1);
+        pow = 1;
+        number = 0;
+        while (pos < str->length()) {
+          number += (pow) * (unsigned)str->at(pos);
+          pow *= 10;
+          ++pos;
+        }
+        res->push_back(number);
+        return res;
+      } else {
+        res->push_back(UINT32_MAX);
+        return res;
       }
     } else {
       return NULL;
@@ -42,8 +76,44 @@ std::vector<unsigned>* Parser_cfg::read_seconds(std::string* str) {
   }
 }
 
-std::vector<unsigned>* read_stream_and_second(std::string* str) {
-  if (!str->find("$")) {
+std::vector<unsigned>* read_stream_and_second(std::string* str,
+                                              std::vector<unsigned>* res) {
+  if (!str || !res) {
+    return NULL;  // null pointer in function
+  }
+  if (str->find("$")) {
+    unsigned number = 0;
+    unsigned pow = 1;
+    size_t pos = str->find("$") + 1;
+    size_t interval = str->find(" ", pos - 1);
+
+    if (pos == interval) {
+      return NULL;  // case if "mix $ ..., need mix $(number) ..."
+    }
+
+    if (str->find(" ", interval) == str->length()) {
+      return NULL;  // case if no seconds after number of stream
+    }
+
+    pos = str->length();
+    while (pos > interval) {
+      number += (pow * (unsigned)str->at(pos));
+      pow *= 10;
+      --pos;
+    }
+
+    res->push_back(number);
+    return res;
+
+  } else {
     return NULL;
   }
+}
+
+char* Parser_cfg::get_conv() {
+  char a = buffer.at(0);
+  std::reverse(buffer.begin(), buffer.end());
+  buffer.pop_back();
+  std::reverse(buffer.begin(), buffer.end());
+  return &a;
 }
--- config_parser.hpp
+++ config_parser.hpp
@@ -6,6 +6,9 @@
 class Parser_cfg {
  private:
   std::ifstream config_file;
+  std::vector<unsigned> mute_seconds;
+  std::vector<std::pair<std::string, unsigned>> stream_seconds_for_mix;
+  std::string buffer;
 
  public:
   Parser_cfg(std::string filename) { config_file.open(filename); };
@@ -15,10 +18,13 @@ class Parser_cfg {
     }
   };
 
-  char* read_conv();
-  std::vector<unsigned>* read_seconds(std::string* str);
-  std::vector<unsigned>* read_stream_and_second(std::string* str);
+  
+  std::vector<unsigned>* read_seconds(std::string* str,
+                                      std::vector<unsigned>* res);
+  std::vector<unsigned>* read_stream_and_second(std::string* str,
+                                                std::vector<unsigned>* res);
   void parse_config_file();
+  char* get_conv();
 };
 
 #endif
