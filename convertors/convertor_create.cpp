658348791277327f08cfc2ba148c4be610ec87a6
phestrix <matrixx.panasx@gmail.com>
2022-12-26 22:36:41 +0700

    Added constructor for convertors

---
+++ convertors/convertor_create.cpp
@@ -0,0 +1,17 @@
+#include <stdexcept>
+
+#include "convertors_interface.hpp"
+#include "mix.hpp"
+#include "mute.hpp"
+
+ConverterPtr ConverterInterface::Create(ConverterCommand converter_comand) {
+  std::string converter_name = converter_comand[0];
+  converter_comand.erase(converter_comand.cbegin());
+  if (converter_name == "mute") {
+    return std::make_unique<MuteConverter>(converter_comand);
+  } else if (converter_name == "mix") {
+    return std::make_unique<MixConverter>(converter_comand);
+  } else {
+    throw std::invalid_argument(converter_name + " : Convertor wasn't exist");
+  }
+}
